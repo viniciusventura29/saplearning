87381ade88712eb89e42f093cdf9bc71685220b2
viniciusventura29 <viniciusventura29@gmail.com>
2023-10-03 22:03:43 -0300

    adding topics

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
-import AuthMiddleware, { SessionUser } from "../authmiddleware/authMiddleware";
+import AuthMiddleware, {
+  SessionUser,
+  supabase,
+} from "../authmiddleware/authMiddleware";
 import { Navbar } from "./components/Navbar";
-import Hero from "./components/hero";
+import Hero from "./components/Hero";
+import { useEffect, useState } from "react";
+import Card from "./components/Card";
 export default function App() {
+  const [topics, setTopics] = useState<any[] | null>();
+
+  useEffect(() => {
+    getTopics().then((e) => {
+      setTopics(e.data);
+    });
+  }, []);
+
+  const getTopics = async () => {
+    return await supabase.from("topics").select("*");
+  };
+
   return (
     <AuthMiddleware>
       {(session: SessionUser) => (
         <div>
           <Navbar session={session} />
           <Hero />
-          {session ? session.user.data.session?.user.email : ""}
+          {session ? <div className="w-full "></div> : <div></div>}
+          <div className="w-full grid gap-4 grid-cols-4 px-52 py-32">
+            {topics?.map((t) => (
+              <Card key={t.title} description={t.description} title={t.title} />
+            ))}
+          </div>
         </div>
       )}
     </AuthMiddleware>
---
+++ src/components/Card.tsx
@@ -0,0 +1,15 @@
+export default function Card({title, description}:{title:string, description:string}) {
+  return (
+    <a
+      href="#"
+      className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+    >
+      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        {title}
+      </h5>
+      <p className="font-normal text-gray-700 dark:text-gray-400">
+        {description}
+      </p>
+    </a>
+  );
+}
