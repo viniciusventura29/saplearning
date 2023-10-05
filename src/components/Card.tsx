277a11a21de6da26601c8ff716cea1f9ae16028d
viniciusventura29 <viniciusventura29@gmail.com>
2023-10-05 16:49:59 -0300

    new editing modal working

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,32 +1,36 @@
 import { useState } from "react";
 import { EditIcon } from "../icons/EditIcon";
 import EditTopicModal from "./EditTopicModal";
+import { SessionUser } from "../../authmiddleware/authMiddleware";
 
 export default function Card({
   title,
   description,
-  id
+  id,
+  session
 }: {
   title: string;
   description: string;
-  id:number
+  id:number,
+  session: SessionUser
 }) {
   const [editTopicModalIsOpen, setEditTopicModalIsOpen] = useState(false);
   return (
     <a
-      href="#"
+      href={`/${title}`}
+      id="anchor"
       className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
     >
       <div className="flex justify-between items-center">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           {title}
         </h5>
-        <button onClick={(e)=>{console.log("OI"); e.preventDefault()}} className="p-1 rounded hover:bg-blue-100"><EditIcon /></button>
+        {session?.user.data.session ? <button onClick={(e)=>{ setEditTopicModalIsOpen(true); e.preventDefault(); e.stopPropagation()}} className="p-1 rounded hover:bg-blue-100"><EditIcon /></button>:null}
       </div>
       <p className="font-normal text-gray-700 dark:text-gray-400">
         {description}
       </p>
-      <EditTopicModal editTopicModalIsOpen={editTopicModalIsOpen} topicDescProps={description} setEditTopicModalIsOpen={setEditTopicModalIsOpen} topicId={id} topicTitleProps={title} />
+      <EditTopicModal editTopicModalIsOpen={editTopicModalIsOpen} topicDescProps={description} setEditTopicModalIsOpen={setEditTopicModalIsOpen} topicId={title} topicTitleProps={title} />
     </a>
   );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ export default function Hero() {
         </p>
         <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
           <a
-            href="#"
+            href="/#anchor"
             className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
           >
             See more
