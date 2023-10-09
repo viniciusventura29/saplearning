c6212d8f7664307f9f2544ac954a680d2d643385
viniciusventura29 <viniciusventura29@gmail.com>
2023-10-09 17:04:13 -0300

    new Article readpage

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,14 +2,14 @@ import boschLogo from "../assets/Bosch_logo.svg";
 import boschSupergraphic from "../assets/Bosch-Supergraphic.svg";
 import { SessionUser } from "../../authmiddleware/authMiddleware";
 import ProfileButton from "./ProfileButton";
-export const Navbar = ({ session }: { session: SessionUser }) => {
+export const Navbar = ({ session }: { session?: SessionUser }) => {
   return (
     <nav className="mb-8 bg-white">
       <div className="h-2 w-full overflow-hidden">
         <img src={boschSupergraphic} className="w-full" alt="" />
       </div>
       <div className="shadow w-full h-20 px-32 py-4 flex items-center justify-between relative">
-        <img className="h-16" src={boschLogo} alt="" />
+       <a href="/"> <img className="h-16" src={boschLogo} alt="" /></a>
         {session?.user.data.session ? <div><ProfileButton session={session} /></div> : <a href="/login" style={{background: "linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)"}} className="rounded p-3 w-36 font-medium uppercase text-xs cursor-pointer flex justify-center text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]">Login</a>}
       </div>
     </nav>
---
+++ src/pages/[id]/index.tsx
@@ -0,0 +1,81 @@
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import { getArticle } from "../../api";
+import { Spinner } from "../../components/Spinner";
+import { Navbar } from "../../components/Navbar";
+import rehypeRaw from "rehype-raw";
+import ReactMarkdown from "react-markdown";
+import AuthMiddleware from "../../../authmiddleware/authMiddleware";
+import { EditIcon } from "../../icons/EditIcon";
+import { Footer } from "../../components/Footer";
+
+function Article({ data, session }: { data: any; session: any }) {
+  return (
+    <div>
+      <Navbar session={session} />
+      <div className="flex mb-32">
+        <div className="w-full h-[100vh - 90px] flex flex-col">
+          <div className="w-full h-14 flex items-center justify-end"></div>
+          <div className="flex justify-center items-center ">
+            <div className="w-2/3">
+              <div className="mb-4 flex justify-between items-center">
+                <h1 className="">
+                  Topic: <span className="font-semibold">{data.topic_id}</span>
+                </h1>
+                {session?.user.data.session ? (
+                  <a
+                    className="flex gap-2 p-2 px-4 bg-blue-500 rounded text-white"
+                    href={`${data.topic_id}/edit`}
+                  >
+                    <EditIcon className="w-4" />
+                    Editar
+                  </a>
+                ) : null}
+              </div>
+              <div className="min-h-screen bg-gray-50 rounded-lg border shadow p-14">
+                <h2 className="text-2xl font-bold mb-6">
+                  {data.title === "" ? (
+                    <span className="text-gray-500 italic">
+                      Article title here
+                    </span>
+                  ) : (
+                    data.title
+                  )}
+                </h2>
+                <ReactMarkdown className="markdown" rehypePlugins={[rehypeRaw]}>
+                  {data.body == "" ? (
+                    "<span className='text-gray-500 italic'>Article body is empty! Start to write something.</span>"
+                  ) : (
+                    data.body
+                  )}
+                </ReactMarkdown>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default function ArticlePage() {
+  const { id } = useParams();
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["getArticle"],
+    queryFn: () => getArticle({ topic_id: id ?? "" }),
+  });
+
+  return (
+    <>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <AuthMiddleware>
+          {(session) => <Article data={data} session={session} />}
+        </AuthMiddleware>
+      )}
+    </>
+  );
+}
