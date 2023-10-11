80b5843c85892152ce966661820035cfde139a99
viniciusventura29 <viniciusventura29@gmail.com>
2023-10-11 16:57:01 -0300

    new Arrow Icons

---
+++ src/icons/ArrowIcon.tsx
@@ -0,0 +1,19 @@
+export const ArrowIcon = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      className={"w-2.5 h-2.5"+className}
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 10 6"
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="m1 1 4 4 4-4"
+      />
+    </svg>
+  );
+};
