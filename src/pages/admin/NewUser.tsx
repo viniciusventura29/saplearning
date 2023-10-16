52330b96e62d16f6467f8e61179122b9704a6e49
viniciusventura29 <viniciusventura29@gmail.com>
2023-10-16 15:44:04 -0300

    new user password warning in new user page

--- src/pages/admin/NewUser.tsx
+++ src/pages/admin/NewUser.tsx
@@ -107,14 +107,19 @@ function NewUser({ session }: { session: SessionUser }) {
               </li>
             </ul>
           </div>
-
           <button
             type="submit"
             className="text-white bg-blue-700 mt-12 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
           >
             Submit new user
           </button>
+          <div className="p-5 bg-yellow-100 rounded-md w-full mt-10">
+                <h3 className="text-yellow-700 mb-1">Warning!</h3>
+                <p className="text-gray-900 mb-5">It's important to know that the user password will be the <i> name+"GSTET3" </i></p>
+                <p><span className="font-bold">Example:</span> Fulano da SilvaGSTET3</p>
+          </div>
         </form>
+        
       </div>
     </div>
   );
